0f74bd6bea00c5296b587189736e127fe5d9b005
swayam-248 <sahoreswayam6862@gmail.com>
2025-07-17 23:27:01 +0530

    Leetcode26

---
+++ L26.java
@@ -0,0 +1,22 @@
+public class L26 {  
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int k = removeDuplicates(nums);
+        System.out.println("k = " + k);
+        System.out.print("Updated nums: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
